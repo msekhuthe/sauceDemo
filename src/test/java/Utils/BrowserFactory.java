c9da2f21be3048f1759360b36892128cfd5187ce
ghost <mogopodisekhuthe@yahoo.co.za>
2025-06-22 17:14:40 +0200

    crreated the browser factory class and method

---
+++ src/test/java/Utils/BrowserFactory.java
@@ -0,0 +1,37 @@
+package Utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Test;
+
+public class BrowserFactory {
+
+    static WebDriver driver;
+
+    public static WebDriver startBrowser(String browserChoice, String url) {
+        if (browserChoice.equalsIgnoreCase("Edge")) {
+            EdgeOptions edgeOptions = new EdgeOptions();
+            //edgeOptions.addArguments("headless");
+            driver = new EdgeDriver(edgeOptions);
+        } else if (browserChoice.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browserChoice.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browserChoice.equalsIgnoreCase("Safari")) {
+            driver = new SafariDriver();
+        } else {
+            throw new IllegalArgumentException("Browser not supported: " + browserChoice);
+        }
+        driver.manage().window().maximize();
+        driver.get(url);
+        return driver;
+    }
+    @Test
+    public void test(){
+        startBrowser("Edge", "https://www.saucedemo.com/");
+    }
+}
