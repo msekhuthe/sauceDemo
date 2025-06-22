c29f80b3397b3410d7da782d38e62c3385fc1f62
ghost <mogopodisekhuthe@yahoo.co.za>
2025-06-22 20:15:44 +0200

    created the Landing page class and method

--- src/test/java/Pages/LandingPage.java
+++ src/test/java/Pages/LandingPage.java
@@ -1,4 +1,31 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
 public class LandingPage {
+
+    WebDriver driver;
+
+    @FindBy(xpath = "//*[@id=\"header_container\"]/div[2]/span")
+    WebElement productText;
+
+    public LandingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyLandingPage(String actualText) {
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(visibilityOf(productText));
+        actualText = productText.getText();
+        Assert.assertEquals(actualText, "Products");
+
+    }
 }
